c22d28d4175f4a52824fbbe34e9070dfeecb60bf
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2020-07-20 16:33:31 -0400

    Update TBS Updater.lua

--- TBS Updater.lua
+++ TBS Updater.lua
@@ -4,10 +4,8 @@ local dir = 'Team-Based-Skins/'
 local url = 'https://raw.githubusercontent.com/Zack2kl/'..dir..'master/'
 local lua, skins, json = 'Team Based Skins.lua', 'skins.txt', 'json.txt'
 
-gui.Command( 'lua.unload ' .. dir..lua )
-
+UnloadScript( dir..lua )
 file.Write( dir..json, http.Get(url..json) )
 file.Write( dir..skins, http.Get(url..skins) )
 file.Write( dir..lua, http.Get(url..lua) )
-
-gui.Command( 'lua.load ' .. dir..lua )
+RunScript( dir..lua )
