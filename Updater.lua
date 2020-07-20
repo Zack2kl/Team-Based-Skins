cbbd9bfeb6dd68dee5f2ec0baf7f70c514e1809c
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2020-07-20 17:17:00 -0400

    Update Updater.lua
    download update script

--- Updater.lua
+++ Updater.lua
@@ -2,10 +2,12 @@
 
 local dir = 'Team-Based-Skins/'
 local url = 'https://raw.githubusercontent.com/Zack2kl/'..dir..'master/'
-local lua, skins, json = 'Team Based Skins.lua', 'skins.txt', 'json.txt'
+local lua, skins, json, cur = 'Team Based Skins.lua', 'skins.txt', 'json.txt', 'Updater.lua'
 
 file.Write( dir..json, http.Get(url..json) )
 file.Write( dir..skins, http.Get(url..skins) )
 file.Write( dir..lua, http.Get(url..lua) )
+file.Write( dir..cur, http.Get(url..cur) )
 
 LoadScript( dir .. lua )
+file.Delete( GetScriptName() )
