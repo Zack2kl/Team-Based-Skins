f063ad279981043326ea5b15fef022d852a32e2d
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2021-01-19 23:07:28 -0500

    Update Random Skins.lua

--- Random Skins.lua
+++ Random Skins.lua
@@ -1,30 +1,28 @@
-local weapons, weapon_keys, skins, JSON = {}, {}, {} do
+local callbacks_Register, client_AllowListener, client_Command, common_Time, file_Read, file_Write, gui_Checkbox, gui_Command, gui_GetValue, gui_Groupbox, gui_Listbox, gui_Multibox, gui_Reference, gui_Slider, http_Get, loadstring, math_random, math_randomseed, unpack, tostring, pcall = callbacks.Register, client.AllowListener, client.Command, common.Time, file.Read, file.Write, gui.Checkbox, gui.Command, gui.GetValue, gui.Groupbox, gui.Listbox, gui.Multibox, gui.Reference, gui.Slider, http.Get, loadstring, math.random, math.randomseed, unpack, tostring, pcall
+
+local weapons, weapon_keys, skins, json do
 	local n = {'json.txt', 'skins.txt'}
-	for i=1,2 do n[i+2]=http.Get('https://raw.githubusercontent.com/Zack2kl/Team-Based-Skins/master/'..n[i])end
 
-	JSON = loadstring(n[3])()
-	local parsed = JSON.parse(n[4])
+	for i=1, 2 do
+		n[i+2] = http_Get('https://raw.githubusercontent.com/Zack2kl/Team-Based-Skins/master/'..n[i])
+	end
 
-	-- Setup weapon tables
-	for l in parsed.item_names:gmatch('([^\n]*)\n')do local n,a=l:match('([^\n]*)='),l:gsub('([^\n]*)=','')weapons[n]=a weapon_keys[#weapon_keys+1]=n end
+	json = loadstring( n[3] )()
+	local parsed = json.parse(n[4])
 
-	-- Setup skin tables
-	local s,t,N=parsed.paintkit_names,{},0
-	for k,v in pairs(parsed.weapon_skins)do local r=tonumber(k)if not t[r]then t[r]={}end for _,b in pairs(v)do table.insert(t[r],s[b]..'='..b)end end
-	local s=''for i=1,#t do s=s..table.concat(t[i],',')..',\n'end
-	for l in s:gmatch('([^\n]*)\n')do skins[N]={}for o in l:gmatch('([^,]*),')do local n,b=o:match('([^=]*)='),o:gsub('([^=]*)=','')table.insert(skins[N],{n,b})end N=N+1 end
+	weapons, weapon_keys, skins = parsed[1], parsed[3], parsed[4]
 end
 
-local ref = gui.Reference('Visuals', 'Skins')
-local group = gui.Groupbox(ref, 'Randomize Skins')
+local ref = gui_Reference('Visuals', 'Skins')
+local group = gui_Groupbox(ref, 'Randomize Skins')
 	group:SetPosX(16) group:SetPosY(528)
 
-local enable = gui.Checkbox(ref, 'random.enable', '', 0)
+local enable = gui_Checkbox(ref, 'random.enable', '', 0)
 	enable:SetPosX(590) enable:SetPosY(544)
 
 local opts = {}
 local opts_l = {}
-local list = gui.Listbox(group, 'random.to_randomize', 200, '')
+local list = gui_Listbox(group, 'random.to_randomize', 200, '')
 	list:SetPosY( 0 )
 	list:SetWidth( 200 )
 
@@ -55,10 +53,10 @@ local function set_disabled(tbl, v)
 	end
 end
 
-local function math_random(...)
-	local seed = math.random(common.Time() * 1000)
-	math.randomseed( seed )
-	return math.random(...)
+local function m_random(...)
+	local seed = math_random(common_Time() * 1000)
+	math_randomseed( seed )
+	return math_random(...)
 end
 
 for i, v in next, weapon_keys do
@@ -68,11 +66,11 @@ for i, v in next, weapon_keys do
 
 	opts_l[i] = v
 	opts[i] = {
-		gui.Checkbox(group, '', 'Random on '.. v, 0),
-		gui.Checkbox(group, '', 'Random Skin', 0),
-		gui.Checkbox(group, '', 'Random Wear', 0),
-		gui.Checkbox(group, '', 'Random Seed', 0),
-		gui.Checkbox(group, '', 'Random Stattrak', 0)
+		gui_Checkbox(group, '', 'Random on '.. v, 0),
+		gui_Checkbox(group, '', 'Random Skin', 0),
+		gui_Checkbox(group, '', 'Random Wear', 0),
+		gui_Checkbox(group, '', 'Random Seed', 0),
+		gui_Checkbox(group, '', 'Random Stattrak', 0)
 	}
 
 	set_visible(opts[i], false)
@@ -85,31 +83,31 @@ for i, v in next, weapon_keys do
 end
 
 list:SetOptions( unpack(opts_l) )
-local multi_1 = gui.Multibox(group, 'Knife Options')
+local multi_1 = gui_Multibox(group, 'Knife Options')
 	multi_1:SetPosX(400) multi_1:SetPosY(0) multi_1:SetWidth(176)
 
 local on_knife = {
-	gui.Checkbox(multi_1, '', 'Enable Random Knife', 0),
-	gui.Checkbox(multi_1, '', 'Random Skin', 0),
-	gui.Checkbox(multi_1, '', 'Random Wear', 0),
-	gui.Checkbox(multi_1, '', 'Random Seed', 0),
-	gui.Checkbox(multi_1, '', 'Random Stattrak', 0)
+	gui_Checkbox(multi_1, '', 'Enable Random Knife', 0),
+	gui_Checkbox(multi_1, '', 'Random Skin', 0),
+	gui_Checkbox(multi_1, '', 'Random Wear', 0),
+	gui_Checkbox(multi_1, '', 'Random Seed', 0),
+	gui_Checkbox(multi_1, '', 'Random Stattrak', 0)
 }
 
-local multi_2 = gui.Multibox(group, 'Gloves Options')
+local multi_2 = gui_Multibox(group, 'Gloves Options')
 	multi_2:SetPosX(400) multi_2:SetPosY(56) multi_2:SetWidth(176)
 local on_glove = {
-	gui.Checkbox(multi_2, '', 'Enable Random Gloves', 0),
-	gui.Checkbox(multi_2, '', 'Random Skin', 0),
-	gui.Checkbox(multi_2, '', 'Random Wear', 0),
-	gui.Checkbox(multi_2, '', 'Random Seed', 0)
+	gui_Checkbox(multi_2, '', 'Enable Random Gloves', 0),
+	gui_Checkbox(multi_2, '', 'Random Skin', 0),
+	gui_Checkbox(multi_2, '', 'Random Wear', 0),
+	gui_Checkbox(multi_2, '', 'Random Seed', 0)
 }
 
-local on_agent = gui.Checkbox(group, '', 'Random Agent Models', 0)
+local on_agent = gui_Checkbox(group, '', 'Random Agent Models', 0)
 	on_agent:SetPosX(400) on_agent:SetPosY(112)
 
-local min_wear = gui.Slider(group, 'random.min_wear', 'Best Wear', 1, 0, 1, 0.001 )
-local max_wear = gui.Slider(group, 'random.max_wear', 'Worst Wear', 0, 0, 1, 0.001 )
+local min_wear = gui_Slider(group, 'random.min_wear', 'Best Wear', 1, 0, 1, 0.001 )
+local max_wear = gui_Slider(group, 'random.max_wear', 'Worst Wear', 0, 0, 1, 0.001 )
 	min_wear:SetDescription('0 is factory-new.') min_wear:SetPosX(216) min_wear:SetPosY(166) min_wear:SetWidth(180)
 	max_wear:SetDescription('1 is battle-scarred.') max_wear:SetPosX(400) max_wear:SetPosY(166) max_wear:SetWidth(180)
 
@@ -117,13 +115,14 @@ local max_wear = gui.Slider(group, 'random.max_wear', 'Worst Wear', 0, 0, 1, 0.0
 local function get_wear()
 	local a = min_wear:GetValue() * 1000
 	local b = max_wear:GetValue() * 1000
-	return math_random( b, a ) / 1000
+	return m_random( b, a ) / 1000
 end
 
 local last_val, last_global, last_global2, last_w, last_k
 local function to_update()
-	local global = gui.GetValue('esp.skins.enable')
+	local global = gui_GetValue('esp.skins.enable')
 	local global_2 = enable:GetValue()
+
 	if global ~= last_global or global_2 ~= last_global2 then
 		group:SetDisabled( not global or not global_2 )
 		last_global = global
@@ -151,24 +150,25 @@ end
 local function add_skin(index, tbl, vanilla)
 	local actual_wep = weapon_keys[index]
 	local weapon_wep = weapons[actual_wep]
+	local ii = tostring(index - 1)
 
 	local vals = get_vals(tbl, true)
 	local random_skin, random_wear, random_seed, random_stat = unpack(vals)
 
 	local ss = vanilla == 1 and 0 or 1
-	local a = skins[index - 1]
-	local r = math_random(ss, #skins[index-1])
+	local a = skins[ii]
+	local r = m_random(ss, #skins[ii])
 	local s = a[r] and a[r][2]
 
 	local str = ('skin.add "%s" "%s" "%s" "%s" "%s" ""'):format(
 		weapon_wep,
 		random_skin and s or '',
 		random_wear and get_wear() or '',
-		random_seed and math_random(0, 1000) or '',
-		random_stat and math_random(0, 10000) or ''
+		random_seed and m_random(0, 1000) or '',
+		random_stat and m_random(0, 10000) or ''
 	)
 
-	gui.Command(str)
+	gui_Command(str)
 end
 
 local function on_event(e)
@@ -180,7 +180,7 @@ local function on_event(e)
 		return
 	end
 
-	gui.Command('skin.clear')
+	gui_Command('skin.clear')
 
 	for i=1, #opts do
 		local el = opts[i]
@@ -191,28 +191,28 @@ local function on_event(e)
 	end
 
 	if on_knife[1]:GetValue() then
-		add_skin(math_random(35, 53), on_knife, 1)
+		add_skin(m_random(35, 53), on_knife, 1)
 	end
 
 	if on_glove[1]:GetValue() then
-		add_skin(math_random(54, 60), on_glove)
+		add_skin(m_random(54, 60), on_glove)
 	end
 
 	if on_agent:GetValue() then
-		local name = weapon_keys[ math_random(61, 101) ]
-		gui.Command( ('skin.add "%s"'):format(weapons[name]) )
+		local name = weapon_keys[ m_random(61, 101) ]
+		gui_Command( ('skin.add "%s"'):format(weapons[name]) )
 	end
 
-	client.Command('cl_fullupdate', true)
+	client_Command('cl_fullupdate', true)
 end
 
-client.AllowListener('round_prestart')
-callbacks.Register('FireGameEvent', on_event)
-callbacks.Register('Draw', to_update)
+client_AllowListener('round_prestart')
+callbacks_Register('FireGameEvent', on_event)
+callbacks_Register('Draw', to_update)
 
 local data = ''
-if pcall(function() data = file.Read('random_skin_options.dat') end) then
-	local options = JSON.parse(data)
+if pcall(function() data = file_Read('random_skin_options.dat') end) then
+	local options = json.parse(data)
 
 	for i=1, #options[1] do
 		set_vals(opts[i], options[1][i])
@@ -225,7 +225,7 @@ if pcall(function() data = file.Read('random_skin_options.dat') end) then
 	max_wear:SetValue(options[6])
 end
 
-callbacks.Register('Unload', function()
+callbacks_Register('Unload', function()
 	local options = {{}, {}, {}, false, 1, 0}
 
 	for i=1, #opts do
@@ -238,5 +238,5 @@ callbacks.Register('Unload', function()
 	options[5] = min_wear:GetValue()
 	options[6] = max_wear:GetValue()
 
-	file.Write( 'random_skin_options.dat', JSON.stringify(options) )
+	file_Write( 'random_skin_options.dat', json.stringify(options) )
 end)
